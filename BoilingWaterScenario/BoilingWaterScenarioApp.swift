a1f8561ba855074b4d116ecbc0048882a5afc98c
gabi brown <gbrown@lcs.on.ca>
2022-02-16 12:01:09 -0500

    Initial Commit

---
+++ BoilingWaterScenario/BoilingWaterScenarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilingWaterScenarioApp.swift
+//  BoilingWaterScenario
+//
+//  Created by gabi brown on 2022-02-16.
+//
+
+import SwiftUI
+
+@main
+struct BoilingWaterScenarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
