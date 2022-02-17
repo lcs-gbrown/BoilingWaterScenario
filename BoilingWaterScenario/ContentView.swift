b965e819894be4179e128124cec9d8a8c983349e
gabi brown <gbrown@lcs.on.ca>
2022-02-16 19:10:24 -0500

    Created initial view with button and text field as well as making the stored and computed property with the formula

--- BoilingWaterScenario/ContentView.swift
+++ BoilingWaterScenario/ContentView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored Properties
+    @State var temperatureEntry = Int("")
+    
+    //MARK: Computed Properties
+    var kpa: String {
+        return String(5 * temperatureEntry! - 400)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("What temperature does your water boil?")
+            
+            TextField("e.g 100",
+                      text: .constant(""),
+                      prompt: Text("e.g. 100"))
+            
+            Button(action: {
+                print("Button was pressed")
+            }, label: {
+                Text("Run formula")
+            })
+                .buttonStyle(.bordered)
+            
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
