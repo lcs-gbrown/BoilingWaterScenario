6931a7979e91bb5005b94cbc46fdf278b73ebb22
gabi brown <gbrown@lcs.on.ca>
2022-02-16 19:52:26 -0500

    changed the formula from a computed property so that the button does the work, got it to print on the view

--- BoilingWaterScenario/ContentView.swift
+++ BoilingWaterScenario/ContentView.swift
@@ -10,27 +10,46 @@ import SwiftUI
 struct ContentView: View {
     
     //MARK: Stored Properties
-    @State var temperatureEntry = Int("")
+    @State var temperatureEntry = ""
+    
+    @State var feedback = ""
+    
+    @State var kpa:Int = 0
     
     //MARK: Computed Properties
-    var kpa: String {
-        return String(5 * temperatureEntry! - 400)
-    }
+    //     var kpa: String {
+    //        return String(5 * Int(temperatureEntry)! - 400)
+    //    }
     
     var body: some View {
         VStack {
             Text("What temperature does your water boil?")
+                .font(.title)
             
-            TextField("e.g 100",
-                      text: .constant(""),
-                      prompt: Text("e.g. 100"))
+            TextField("",
+                      text: $temperatureEntry)
+                .multilineTextAlignment(.leading)
             
             Button(action: {
-                print("Button was pressed")
+                kpa = (5 * Int(temperatureEntry)! - 400)
+                
+                print(kpa)
+                
+                if kpa < 99 {
+                    feedback = "Above sea level"
+//                } else if (kpa != 0) : 100 {
+//                    feedback = "Sea level"
+                    
+                } else if kpa > 101 {
+                    feedback = "Below sea level"
+                }
+                
             }, label: {
                 Text("Run formula")
             })
                 .buttonStyle(.bordered)
+            
+            Text("\(kpa)")
             
         }
     }
